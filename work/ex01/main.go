f346ae1ad4d0d87d4b5aeac684d97ea71228f957
github kkk777-7 <kota.kimura0725@gmail.com>
2021-11-07 00:20:32 +0900

    Added check format

--- work/ex01/main.go
+++ work/ex01/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"gopher-dojo/work/ex01/convert"
 	"log"
 )
@@ -16,9 +17,41 @@ func init() {
 }
 
 func main() {
-	i := convert.CreateImageFormat(flag.Args()[0], PreFormat, AfterFormat)
-	err := i.Convert()
-	if err != nil {
-		log.Fatal(err)
+	if len(flag.Args()) == 0 {
+		log.Fatal("Please input the target path!")
 	}
+
+	chk := checkFormat(flag.Args()[0], PreFormat, AfterFormat)
+	if chk {
+		i := convert.CreateImageFormat(flag.Args()[0], PreFormat, AfterFormat)
+		err := i.Convert()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+}
+
+func checkFormat(path string, pre string, after string) bool {
+	var prechk bool = false
+	var afterchk bool = false
+	var chk bool = true
+
+	extensions := []string{"png", "jpg", "jpeg", "gif"}
+	for _, extension := range extensions {
+		if PreFormat == extension {
+			prechk = true
+		}
+		if AfterFormat == extension {
+			afterchk = true
+		}
+	}
+	if !prechk {
+		chk = false
+		fmt.Println("The pre extension is incorrect")
+	}
+	if !afterchk {
+		chk = false
+		fmt.Println("The after extension is incorrect")
+	}
+	return chk
 }
