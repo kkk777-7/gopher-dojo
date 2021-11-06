897a8169caf105cd97560a31c24f68b06a8182ae
github kkk777-7 <kota.kimura0725@gmail.com>
2021-11-06 17:54:29 +0900

    Added recursive search files.

--- work/ex01/convert/convert.go
+++ work/ex01/convert/convert.go
@@ -7,40 +7,83 @@ import (
 	"image/jpeg"
 	"image/png"
 	"os"
+	"path/filepath"
 )
 
-func Convert(st string, dst string, pre string, after string) error {
+type ImageFormat struct {
+	Path        string
+	PreFormat   string
+	AfterFormat string
+}
 
-	file, err := os.Open(st)
+func (i ImageFormat) Convert() error {
+	files, err := searchfile(i.Path, i.PreFormat)
 	if err != nil {
 		return err
 	}
-	defer file.Close()
+	//fmt.Println(files)
 
-	img, _, err := image.Decode(file)
-	if err != nil {
-		return err
-	}
-	out, err := os.Create(dst)
-	if err != nil {
-		return err
-	}
-	defer out.Close()
+	for _, file := range files {
+		fs, err := os.Open(file)
+		if err != nil {
+			return err
+		}
+		defer fs.Close()
 
-	switch after {
-	case "jpg":
-		err = jpeg.Encode(out, img, nil)
-	case "jpeg":
-		err = jpeg.Encode(out, img, nil)
-	case "png":
-		err = png.Encode(out, img)
-	case "gif":
-		err = gif.Encode(out, img, nil)
-	}
-	if err != nil {
-		return err
-	}
-	fmt.Printf("convert: %s\n", out.Name())
+		img, _, err := image.Decode(fs)
+		if err != nil {
+			return err
+		}
 
+		dstpath := changeSuffixFile(file, i.AfterFormat)
+		//fmt.Println(dstpath)
+		out, err := os.Create(dstpath)
+		if err != nil {
+			return err
+		}
+		defer out.Close()
+
+		switch i.AfterFormat {
+		case "jpg":
+			err = jpeg.Encode(out, img, nil)
+		case "jpeg":
+			err = jpeg.Encode(out, img, nil)
+		case "png":
+			err = png.Encode(out, img)
+		case "gif":
+			err = gif.Encode(out, img, nil)
+		}
+		if err != nil {
+			return err
+		}
+		fmt.Printf("convert: %s\n", out.Name())
+	}
 	return nil
+}
+
+func CreateImageFormat(path string, pre string, after string) *ImageFormat {
+	res := new(ImageFormat)
+	res.Path = path
+	res.PreFormat = pre
+	res.AfterFormat = after
+	return res
+}
+
+func searchfile(path string, format string) ([]string, error) {
+	var files []string
+
+	err := filepath.Walk(path,
+		func(path string, info os.FileInfo, err error) error {
+			if filepath.Ext(path) == "."+format {
+				files = append(files, path)
+			}
+			return nil
+		})
+	return files, err
+}
+
+func changeSuffixFile(file string, format string) string {
+	suffix := filepath.Ext(file)
+	newfile := file[:len(file)-len(suffix)+1] + format
+	return newfile
 }
--- work/ex01/main.go
+++ work/ex01/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"flag"
-	"fmt"
 	"gopher-dojo/work/ex01/convert"
 	"log"
 )
@@ -11,14 +10,14 @@ var PreFormat string
 var AfterFormat string
 
 func init() {
-	flag.StringVar(&PreFormat, "pre", "jpeg", "pre image format")
-	flag.StringVar(&AfterFormat, "after", "png", "after image format")
+	flag.StringVar(&PreFormat, "p", "jpg", "pre image format")
+	flag.StringVar(&AfterFormat, "a", "png", "after image format")
 	flag.Parse()
 }
 
 func main() {
-	fmt.Println(PreFormat, AfterFormat, flag.Args()[0], flag.Args()[1])
-	err := convert.Convert(flag.Args()[0], flag.Args()[1], PreFormat, AfterFormat)
+	i := convert.CreateImageFormat(flag.Args()[0], PreFormat, AfterFormat)
+	err := i.Convert()
 	if err != nil {
 		log.Fatal(err)
 	}
