52debc64243e96cb6a84bfa5d41a0935db8702ff
github kkk777-7 <kota.kimura0725@gmail.com>
2021-11-06 14:55:15 +0900

    Added flag

--- work/ex01/main.go
+++ work/ex01/main.go
@@ -1,7 +1,25 @@
 package main
 
-import "gopher-dojo/work/ex01/convert"
+import (
+	"flag"
+	"fmt"
+	"gopher-dojo/work/ex01/convert"
+	"log"
+)
+
+var PreFormat string
+var AfterFormat string
+
+func init() {
+	flag.StringVar(&PreFormat, "pre", "jpeg", "pre image format")
+	flag.StringVar(&AfterFormat, "after", "png", "after image format")
+	flag.Parse()
+}
 
 func main() {
-	convert.Convert()
+	fmt.Println(PreFormat, AfterFormat, flag.Args()[0], flag.Args()[1])
+	err := convert.Convert(flag.Args()[0], flag.Args()[1], PreFormat, AfterFormat)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
