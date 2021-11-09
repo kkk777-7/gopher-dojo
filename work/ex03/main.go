fa56d1e238b4b4e7a663ebf0610ee9d71f5849f7
github kkk777-7 <kota.kimura0725@gmail.com>
2021-11-10 01:13:09 +0900

    Updated main.go

--- work/ex03/main.go
+++ work/ex03/main.go
@@ -1,29 +1,14 @@
 package main
 
 import (
-	"fmt"
+	"flag"
 	"gopher-dojo/work/ex03/typing"
-	"os"
 	"time"
 )
 
 func main() {
-	var cnt int = 0
-	fmt.Println("Start!!!")
-	ch := typing.Input(os.Stdin)
-	for {
-		fmt.Print("test->")
-		select {
-		case res := <-ch:
-			chk := typing.CheckInput("test", res)
-			if chk {
-				cnt++
-			}
-		case <-time.After(5 * time.Second):
-			fmt.Println()
-			fmt.Println("Finish!!")
-			fmt.Printf("Your Score: %v\n", cnt)
-			return
-		}
-	}
+	n := flag.Duration("t", 30*time.Second, "Time of typing game")
+	flag.Parse()
+	typing.Typing(n)
+
 }
